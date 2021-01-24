7f3533dd85cc369f950801e643b073114a5bed00
Samriddh Singh <samriddhsingh00@gmail.com>
2021-01-25 02:08:07 +0530

    next 2

---
+++ DCS_DSA/negative to one side/cmake-build-debug/CMakeFiles/negative_to_one_side.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/negative_to_one_side.dir/main.cpp.obj"
+  "libnegative_to_one_side.dll.a"
+  "negative_to_one_side.exe"
+  "negative_to_one_side.exe.manifest"
+  "negative_to_one_side.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/negative_to_one_side.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
