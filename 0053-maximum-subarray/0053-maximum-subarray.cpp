0575bf8d0d59374835b9e625c355f0f8ecdb804a
Samriddh <56023229+saketbyte@users.noreply.github.com>
2022-11-07 19:55:40 +0530

    Time: 412 ms (5.07%), Space: 67.8 MB (11.98%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,14 +6,14 @@ public:
         
         int curr_max = 0;
         int max_so_far = INT_MIN;
-        int al_neg = INT_MIN;
+   
         
         for(int i = 0; i<n;i++)
         {
-            al_neg = max(al_neg, nums[i]);
+            
             curr_max += nums[i];
             
-            if(curr_max>max_so_far)
+            if(curr_max > max_so_far)
                 max_so_far = curr_max;
             
             if(curr_max < 0)
@@ -21,7 +21,7 @@ public:
             
         }
         
-        return max(max_so_far,al_neg);
+        return max_so_far;
         
         
     }
