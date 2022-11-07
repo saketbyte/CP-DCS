b6c89db36432180f7bc49a53d04ea3c38122dcb7
Samriddh <56023229+saketbyte@users.noreply.github.com>
2022-11-07 20:14:52 +0530

    Time: 177 ms (34.12%), Space: 40 MB (20.18%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -3,6 +3,12 @@ public:
     int maxSubarraySumCircular(vector<int>& A) {
         
         int total = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
+        
+        /*
+        The two possible cases here are:
+        1. maxSum : The normal sum which is simply the max sum subarray in the array sequentially.
+        2. total-minSu: The sum which is spread across two ends of the subarray, one part on the left end and the other on the right end. So to find for case 2, we are just avoiding the dip of minimum subarray in the middle.
+        */
         for (int& a : A) {
             curMax = max(curMax + a, a);
             maxSum = max(maxSum, curMax);
