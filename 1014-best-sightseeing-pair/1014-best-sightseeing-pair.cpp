778160656a82d73bbc5fa4048dbf6a5428c1d9af
Samriddh <56023229+saketbyte@users.noreply.github.com>
2022-11-08 12:11:08 +0530

    Time: 121 ms (55.41%), Space: 39.3 MB (84.34%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -8,12 +8,17 @@ public:
         // Make sure to not chosee when i == j
         
         int n = A.size();
+        // Ek step pehle ye kar dia start update
         int start =A[0] + 0;
         int res = INT_MIN;
         
         for(int i =1;i<n;i++)
         {
+            // fir yahan par A[i]-i or sum calculate kia or max store kia
             res = max(res, start + A[i]-i);
+            
+            // uske baad yahan par start calculate kiya
+            
             start = max(start, A[i]+i);
             
         }
