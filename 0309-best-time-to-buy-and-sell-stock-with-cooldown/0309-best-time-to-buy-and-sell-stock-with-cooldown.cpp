a082c7f16036b07156e00141cd4d8dcb71228a66
Samriddh <56023229+saketbyte@users.noreply.github.com>
2022-11-09 12:48:33 +0530

    Time: 13 ms (29.54%), Space: 11.9 MB (48.60%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,34 +1,49 @@
 class Solution {
 public:
     
-    int getAns(vector<int> Arr, int ind, int buy, int n, vector<vector<int>> &dp ){
+//     int getAns(vector<int> Arr, int ind, int buy, int n, vector<vector<int>> &dp ){
 
-    if(ind>=n) return 0; //base case
+//     if(ind>=n) return 0; //base case
     
-    if(dp[ind][buy]!=-1)
-        return dp[ind][buy];
+//     if(dp[ind][buy]!=-1)
+//         return dp[ind][buy];
         
-    int profit;
+//     int profit;
     
-    if(buy==0){// We can buy the stock
-        profit = max(0+getAns(Arr,ind+1,0,n,dp), -Arr[ind]+getAns(Arr,ind+1,1,n,dp));
-    }
+//     if(buy==0){// We can buy the stock
+//         profit = max(0+getAns(Arr,ind+1,0,n,dp), -Arr[ind]+getAns(Arr,ind+1,1,n,dp));
+//     }
     
-    if(buy==1){// We can sell the stock
-        profit = max(0+getAns(Arr,ind+1,1,n,dp), 
-                     Arr[ind]+getAns(Arr,ind+2,0,n,dp));// minor change
-    }
+//     if(buy==1){// We can sell the stock
+//         profit = max(0+getAns(Arr,ind+1,1,n,dp), 
+//                      Arr[ind]+getAns(Arr,ind+2,0,n,dp));// minor change
+//     }
     
-    return dp[ind][buy] = profit;
-}
+//     return dp[ind][buy] = profit;
+// }
     
     int maxProfit(vector<int>& Arr) {
         
         int n = Arr.size();
-    vector<vector<int>> dp(n,vector<int>(2,-1));
+    vector<vector<int>> dp(n+2,vector<int>(2,0));
     
-    int ans = getAns(Arr,0,0,n,dp);
-    return ans;
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            int profit;
+            
+            if(buy==0){// We can buy the stock
+                profit = max(0+dp[ind+1][0], -Arr[ind] + dp[ind+1][1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+dp[ind+1][1], Arr[ind] + dp[ind+2][0]);
+            }
+            
+            dp[ind][buy] = profit;
+        }
+    }
+    
+    return dp[0][0];
         
     }
 };
