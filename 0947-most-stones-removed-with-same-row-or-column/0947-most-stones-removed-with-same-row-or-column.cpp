883548072ffd5483fc8808359aa738fe23246740
Samriddh <56023229+saketbyte@users.noreply.github.com>
2022-11-14 06:25:20 +0530

    Time: 269 ms (51.85%), Space: 14.2 MB (88.33%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,37 @@
+      
+        // https://youtu.be/OwMNX8SPavM I do not understand welll. I have not studied graphs
+        // I will submit for the sake of it for now, but I will study this sooon. 
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        const int n = stones.size();
+        vroot = vector<int>(n);
+        for(int i=0;i<n;i++) vroot[i] = i;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
+                    connect(i,j);
+            }
+        }
+        int cnt = 0;
+        for(int i=0;i<n;i++) 
+            if(vroot[i] == i) cnt++;
+        return n - cnt;
+    }
+    
+    int getRoot(int i){
+        if(vroot[i] != i){
+            vroot[i] = getRoot(vroot[i]);
+        }
+        return vroot[i];
+    }
+    
+    void connect(int i, int j){
+        vroot[getRoot(i)] = getRoot(j); 
+    }
+    
+    vector<int> vroot;
+};
+        
+        
