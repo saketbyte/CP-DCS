bb137a6b34ae1089ec0ab78d0fe5bf507ae1e668
Samriddh <56023229+saketbyte@users.noreply.github.com>
2022-11-22 16:42:03 +0530

    Time: 147 ms (87.49%), Space: 29.9 MB (86.87%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,48 +1,49 @@
 class Solution {
 public:
-    int nearestExit(vector<vector<char>>& maze, vector<int>& e) {
-        queue<pair<int,int>> q;
-        q.push({e[0],e[1]});
-		
-		//current moves
-        int moves=1;
-        int rows=maze.size();
-        int cols=maze[0].size();
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+       int m = maze[0].size(),n = maze.size();
+        queue<pair<int,int>>q;
         
-		//to move in all directions
-        vector<vector<int>> offsets={{0,1},{1,0},{0,-1},{-1,0}};
-		
-        //mark the entrance  as visited
-        maze[e[0]][e[1]]='+';
+        q.push({entrance[0],entrance[1]});
+        maze[entrance[0]][entrance[1]] = '+';
+        int level = 0;
+        int dr[4] = {1 , -1 ,  0 , 0};
+        int dc[4] = {0 ,  0 , -1 , 1};
+        
         while(!q.empty())
         {
-            int l=q.size();
-			//for every node in the queue visit all of it's adjacent nodes which are not visited yet
-            for(int k=0;k<l;k++)
+            int size = q.size();
+            level++;
+            for(int i=0;i<size;i++)
             {
-                auto [i,j]=q.front();
+                auto x = q.front();
                 q.pop();
-                
-				//try all 4 directions from the current cell
-                for(int l=0;l<4;l++)
+                int row = x.first;
+                int col = x.second;
+                for(int j=0;j<4;j++)
                 {
-                    int x=i+offsets[l][0];
-                    int y=j+offsets[l][1];
-					//a invalid move
-                    if(x<0 || y<0 || x>=rows || y>=cols || maze[x][y]=='+')
+                    int new_row = row + dr[j];
+                    int new_col = col + dc[j];
+                    if(new_row>= 0 && new_col >=0 && new_row<n && new_col<m)
+                    {
+                    
+                    if(maze[new_row][new_col] == '+')
+                    {
                         continue;
-					//if we have reached the exit then current moves are the min moves to reach the exit
-                    if(x==0 || y==0 || x==rows-1 || y==cols-1)
-                        return moves;
-					//block the cell as we have visited
-                    maze[x][y]='+';
-                    q.push({x,y});
+                    }
+                        
+                    if(new_row == 0 or new_col == 0 or new_row == n-1 or new_col == m-1)
+                    {
+                        return level;
+                    }
+                    q.push({new_row,new_col});
+                    maze[new_row][new_col] = '+'; 
+                } 
                 }
             }
-			//increment the moves
-            moves++;
-            
         }
-        return -1;
+        
+        
+     return -1;   
     }
 };
