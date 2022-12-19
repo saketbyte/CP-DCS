24a73e3d4a8f18dd5f6e72b3078c3294abb45496
Samriddh <56023229+saketbyte@users.noreply.github.com>
2022-12-19 13:28:51 +0530

    Time: 17 ms (10.95%), Space: 16.7 MB (7.63%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -13,21 +13,53 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         
-        if(root==NULL) return true; //Tree is empty
-        
-        return isSymmetricTest(root->left,root->right);
+         return isSymmetricTest(root->left,root->right);
     }
     
     bool isSymmetricTest(TreeNode* p , TreeNode* q){
-        if(p == NULL && q == NULL) //left & right node is NULL 
-            return true; 
         
-        else if(p == NULL || q == NULL) //one of them is Not NULL
-            return false; 
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL)return false;
+        if(q == NULL) return false;
+        queue<TreeNode*>q1, q2;
         
-        else if(p->val!=q->val) 
-            return false;
+        q1.push(p);
+        q2.push(q);
+        while(!q1.empty() || !q2.empty()){
+            TreeNode* a1 = q1.front();
+            TreeNode* a2 = q2.front();
+            
+            if(a1-> val != a2->val) return false; // not a same tree
+            q1.pop();
+            q2.pop();
+            //check for there left and right subtrees 
+            if(a1->left  && a2->right ){
+                q1.push(a1->left);
+                q2.push(a2->right);
+            }
+            else if(a1->left || a2->right){
+                return false;
+                
+            }
+            if(a1->right && a2->left){
+                q1.push(a1->right);
+                q2.push(a2->left);
+            }
+            else if ( a1->right || a2->left){
+                 return false;
+            }
+        }
+        return true;
         
-        return isSymmetricTest(p->left,q->right) && isSymmetricTest(p->right,q->left); //comparing left subtree's left child with right subtree's right child --AND-- comparing left subtree's right child with right subtree's left child
+//         if(p == NULL && q == NULL) //left & right node is NULL 
+//             return true; 
+        
+//         else if(p == NULL || q == NULL) //one of them is Not NULL
+//             return false; 
+        
+//         else if(p->val!=q->val) 
+//             return false;
+        
+//         return isSymmetricTest(p->left,q->right) && isSymmetricTest(p->right,q->left); //comparing left subtree's left child with right subtree's right child --AND-- comparing left subtree's right child with right subtree's left child
     }
 };
