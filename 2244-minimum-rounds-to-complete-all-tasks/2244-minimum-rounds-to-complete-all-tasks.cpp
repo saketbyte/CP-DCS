7af51915d6cfa8e447ec7c7bf5a014f570cab4c0
Samriddh <56023229+saketbyte@users.noreply.github.com>
2023-01-04 23:39:33 +0530

    Time: 486 ms (44.73%), Space: 103.7 MB (72.00%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,49 @@
+/*
+Intuition
+If the frequence freq of a level is 1,
+then it is not possible to complete all the tasks.
+
+Otherwise, we need to decompose freq = 3 tasks + 3 tasks + .... + 2 tasks,
+with the minimum number of 3 and 2.
+
+We need a lot a 3-tasks, and plus one or two 2-tasks.
+
+
+Explanation
+Tasks with same difficulty level can be done together,
+in group of 2-tasks or 3-tasks.
+
+So we count the frequnce freq for each level.
+
+If freq = 1, not possible, return -1
+If freq = 2, needs one 2-tasks
+If freq = 3, needs one 3-tasks
+If freq = 3k, freq = 3 * k, needs k batchs.
+If freq = 3k + 1, freq = 3 * (k - 1) + 2 + 2, needs k + 1 batchs.
+If freq = 3k + 2, freq = 3 * k + 2, needs k + 1 batchs.
+
+To summarize, needs (freq + 2) / 3 batch,
+return the sum of (freq + 2) / 3 if possible.
+*/
+
+
+
+
+
+
+
+class Solution {
+public:
+    int minimumRounds(vector<int>& A) {
+        unordered_map<int, int> count;
+        int res = 0, freq1;
+        for (int a: A)
+            ++count[a];
+        for (auto& it: count) {
+            if (it.second == 1) return -1;
+            res += (it.second + 2) / 3;
+        }
+        return res;
+        
+    }
+};
