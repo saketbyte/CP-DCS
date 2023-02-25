731e74eef85b3fe8eda16bb7f77143f3bf3ee66b
Samriddh <56023229+saketbyte@users.noreply.github.com>
2023-02-25 23:06:01 +0530

    Time: 122 ms (94.89%), Space: 93.4 MB (11.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int ans = 0;
         int buy = A[0];
         
-        for(int i =1;i<A.size();i++)
+        for(int i = 1;i<A.size() ; i++)
         {
             ans = max(ans, A[i]-buy);
             buy = min(buy, A[i]);
