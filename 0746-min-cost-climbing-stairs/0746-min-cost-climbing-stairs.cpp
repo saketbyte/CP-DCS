d0fd3f17a463a571fb852fae23107a22f0f12015
Samriddh <56023229+saketbyte@users.noreply.github.com>
2023-04-04 18:08:51 +0530

    Time: 9 ms (37.83%), Space: 13.8 MB (34.73%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,28 +1,31 @@
 class Solution {
-public:   
-	int solve(vector <int> &cost, vector<int> &dp, int i)
-    {
-        
-        if(i >= size(cost)) return 0;
-        
-	if(dp[i]) return dp[i]; 
-        // if already calculated, directly return stored minimum cost
-	return dp[i] = 
-        cost[i] + min(solve(cost, dp, i + 1), solve(cost, dp, i + 2));
+public:  
+    
+    
+// 	int solve(vector <int> &cost, vector<int> &dp, int i)
+//     {
+      
+//         if (i < 0) return 0;
+// 	if (i==0 || i==1) return cost[i];
         
         
-    }
+// 	if(dp[i]) return dp[i]; 
+        
+// 	return dp[i] = 
+//         cost[i] + min(solve(cost, dp, i -2), solve(cost, dp, i -1));
+        
+//     }
 
     int minCostClimbingStairs(vector<int>&cost) {
         
         vector<int> dp(cost.size(), 0);
         
-        return min(solve(cost, dp, 0), solve(cost, dp, 1));
+//         return min(solve(cost, dp, cost.size()-1), solve(cost, dp, cost.size()-2));
         
-    // for(int i=2;i<cost.size();i++)
-    //     {
-    //         cost[i]+=min(cost[i-1],cost[i-2]);
-    //     }
-    //     return min(cost[cost.size()-1],cost[cost.size()-2]);
+    for(int i=2;i<cost.size();i++)
+        {
+            cost[i]+=min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[cost.size()-1],cost[cost.size()-2]);
     }
 };
