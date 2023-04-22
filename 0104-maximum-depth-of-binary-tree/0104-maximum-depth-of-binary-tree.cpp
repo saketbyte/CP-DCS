b97be25853effe9c903c6bea8fb8746999995797
Samriddh <56023229+saketbyte@users.noreply.github.com>
2023-04-22 18:26:39 +0530

    Time: 16 ms (14.57%), Space: 19 MB (19.26%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,12 +11,47 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        
-        if(root==NULL)
+   int maxDepth(TreeNode* root) 
+    {
+		// If root is NULL
+        if(!root)
             return 0;
-     
-            return 1+ max(maxDepth(root->left),maxDepth(root->right));
         
+		// Create a variable to store the maximum depth
+        int height = 0;
+        
+		// Create a queue
+        queue<TreeNode*> q;
+		
+		// Push root to the queue
+        q.push(root);
+        
+		// Loop until queue is not empty
+        while(!q.empty())
+        {
+			// Increment height
+            height++;
+			
+			// Total nodes in the current level IMPORTANT
+            int n = q.size();
+            
+			// Loop for each level and push into queue untill the level is traced entirely. Once done, we move to next level. 
+            while(n--)
+            {
+                TreeNode* curr = q.front();
+                q.pop();
+                
+				// If left child exists
+                if(curr->left)
+                    q.push(curr->left);
+                
+				// If right child exists
+                if(curr->right)
+                    q.push(curr->right);
+            }
+        }
+        
+		// Return maximum depth
+        return height;
     }
 };
