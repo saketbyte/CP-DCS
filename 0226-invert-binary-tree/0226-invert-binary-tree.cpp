8ea045be0242728f35279a3ff4457edc829bddc4
Samriddh <56023229+saketbyte@users.noreply.github.com>
2023-04-23 18:00:13 +0530

    Time: 3 ms (60.30%), Space: 9.9 MB (6.39%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -15,14 +15,19 @@ public:
         
        
         
-        if(root)
-        {
-            invertTree(root->left);
-            invertTree(root->right);
-            std::swap(root->left, root->right);
+       std::stack<TreeNode*> stk;
+    stk.push(root);
+    
+    while (!stk.empty()) {
+        TreeNode* p = stk.top();
+        stk.pop();
+        if (p) {
+            stk.push(p->left);
+            stk.push(p->right);
+            std::swap(p->left, p->right);
         }
-        
-        return root;
+    }
+    return root;
             
     }
 };
