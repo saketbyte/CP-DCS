c148154bf5ffeb1b5ceb5b488322a1f70432325d
Samriddh <56023229+saketbyte@users.noreply.github.com>
2023-05-08 12:41:42 +0530

    Time: 55 ms (72.79%), Space: 41.8 MB (78.97%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,8 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    const transformedArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    transformedArr[i] = fn(arr[i], i);
-  }
-  return transformedArr;
+    for (let i = 0; i < arr.length; ++i) {
+        arr[i] = fn(arr[i], i);
+    }
+    return arr;
 };
