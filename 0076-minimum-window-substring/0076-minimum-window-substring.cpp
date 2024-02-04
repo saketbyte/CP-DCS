9f3536eca6d55d94c9a6fdd98605b2604e83e8b8
Samriddh <56023229+saketbyte@users.noreply.github.com>
2024-02-04 06:20:14 +0530

    Time: 6 ms (85.94%), Space: 9.4 MB (20.30%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,66 +1,16 @@
 class Solution {
 public:
-     
-       
-  string minWindow(string s, string t) {
-    
-    //This result variable will store the string which we will return
-    string result;
-
-    //It will check if any of the two string is empty and return empty string result
-    if(s.empty() || t.empty()){
-        return result;
-    }
-
-    //These 2 unorders maps will store frequency of characters in t string and windows
-    unordered_map<char, int> map;
-    unordered_map<char, int> window;
-
-    //It will update the frequency of each charater of string t int map;
-    for(int i = 0; i < t.length(); i++){
-        map[t[i]]++;
-    }
-
-    //variable to store minimum length of result string
-    int minLength = INT_MAX;
-
-    //counter
-    int letterCounter = 0;
-    
-    for(int slow = 0, fast = 0; fast < s.length(); fast++){
-        
-        //This c variable will store each character of string s according to the fast pointer movement starting from 0
-        char c = s[fast];
-
-        //If current character of "s" is available in map that consists of string "t" characters
-        if(map.find(c) != map.end()){
-            //store that character in current window and increase the counter by 1
-            window[c]++;
-            //This if condition will execute only if there is need to fulfill required charters from map to window and increase the letter counter
-            if(window[c] <= map[c]){
-                letterCounter++;
-            }
+    string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for(auto c: t) map[c]++;
+        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
+        while(end<s.size()){
+            if(map[s[end++]]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s[begin++]]++==0) counter++;  //make it invalid
+            }  
         }
-
-        //This condition we have found the result in current window
-        if(letterCounter >= t.length()){
-            while (map.find(s[slow]) == map.end() || window[s[slow]] > map[s[slow]])
-            {
-                //This will start decreasing frequency of extra character in window
-                window[s[slow]]--;
-
-                //and move slow pointer
-                slow++;
-            }
-
-            //It will update the minimum required substring
-            if(fast-slow + 1 < minLength){
-                minLength = fast - slow + 1;
-                //It will update the result string
-                result = s.substr(slow, minLength);
-            }
-        }
+        return d==INT_MAX? "":s.substr(head, d);
     }
-    return result;
-}
 };
