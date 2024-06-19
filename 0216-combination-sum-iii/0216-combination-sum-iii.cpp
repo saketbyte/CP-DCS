b80ce12fa26c748c09a98ebd7d7d15716629b7ab
Samriddh <56023229+saketbyte@users.noreply.github.com>
2024-06-19 14:17:33 +0530

    Time: 3 ms (49.70%), Space: 8.4 MB (35.78%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     
-void findCombinations(int start, int k, int n, std::vector<int>& combination, std::vector<std::vector<int>>& result) {
-    // Base case: if k is 0 and n is 0, we have found a valid combination
-    if (k == 0 && n == 0) {
-        result.push_back(combination);
-        return;
+    void generate(int k, int n, int start, int sum, vector<vector<int>> &ans, vector<int> &sub)
+    {
+        if(k==0 && n == sum)
+        {
+            ans.push_back(sub);
+            return;
+        }
+        
+        if(k<0 || sum>n) return;
+        
+        if(start<10)
+        {
+            sub.push_back(start);
+            generate(k-1,n,start+1,sum+start,ans, sub);
+            sub.pop_back();
+            generate(k,n,start+1,sum,ans, sub);
+
+        }
+
     }
-    
-    // If k or n becomes negative, there's no valid combination
-    if (k < 0 || n < 0) {
-        return;
-    }
-    
-    // Try to pick the number 'start'
-    if (start <= 9) {
-        // Pick 'start'
-        combination.push_back(start);
-        findCombinations(start + 1, k - 1, n - start, combination, result);
-        // Not pick 'start'
-        combination.pop_back();
-        findCombinations(start + 1, k, n, combination, result);
-    }
-}
-    
     vector<vector<int>> combinationSum3(int k, int n) {
+        
         vector<vector<int>> ans;
         vector<int> sub;
-        findCombinations(1, k, n, sub, ans);
+        
+        generate(k,n,1,0,ans, sub);
+        
         return ans;
+        
     }
 };
