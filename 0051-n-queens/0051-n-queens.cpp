a16f79484955bf4f9b271af040c5c52759c4a4db
Samriddh <56023229+saketbyte@users.noreply.github.com>
2024-06-26 17:22:28 +0530

    Time: 12 ms (23.16%), Space: 12.6 MB (23.51%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,75 +1,47 @@
 class Solution {
 public:
-     vector<vector<string>> result;
-    bool isValid(vector<string>& board, int row, int col) {
-        //look for up
-        for(int i = row; i>=0; i--) {
-            if(board[i][col] == 'Q')
-                return false;
-        }
-        
-        //check left diagonal upwards
-        for(int i = row, j = col; i>=0 && j >= 0; i--, j--) {
-            if(board[i][j] == 'Q')
-                return false;
-        }
-        
-        //check right diagonal upwards
-        for(int i = row, j = col; i>=0 && j<board.size(); i--, j++) {
-            if(board[i][j] == 'Q')
-                return false;
-        }
-        
-        /*
-			Wait a second, Why didn't I check any squares downwards ???
-			If you notice, every time I am calling dfs(board, row+1); i.e. after
-			placing a Queen at a row, I move down. So, It's guaranteed I will
-			not get any Queen downwards.
-			Example :
-
-			For n = 4
-
-	                _   _    _  Q   (Put int the first row)
-	                Q  _   _   _    (While putting here, I only need to see above of me because I have not populated any Q in downwards)
-	                _   _   Q  _    (Same,  While putting here, I only need to see above of me because I have not populated any Q in downwards)
-	
-	                So, on
-
-	*/
-        
-        
-        return true;
-    }
-    void solve(vector<string>& board, int row) {
+    vector<vector<string>> result;
+    void solve(vector<string>& board, int row, unordered_set<int>& cols, unordered_set<int>& diags, unordered_set<int>& antiDiags) {
         if(row == board.size()) {
             result.push_back(board);
             return;
         }
         
-        /*
-            place one queen at every row and check before placing
-            in every directions where there is risk if being attackes
-            i.e. up, diagonally because we are placing queens from
-            top row to bottom row, so we need to check if we put a queen
-            vertically up in some row or diagonally upwards in some row
-        */
-        for(int i = 0; i<board.size(); i++) {
-            if(isValid(board, row, i)) {
-                board[row][i] = 'Q';
-                
-                solve(board, row+1);
-                
-                board[row][i] = '.';
-            }
+       
+        for(int col = 0; col<board.size(); col++) {
+            int diag_id       = row-col;
+            int anti_diag_id  = row+col;
+            
+           
+            if(cols.find(col) != cols.end() ||
+              diags.find(diag_id) != diags.end() ||
+              antiDiags.find(anti_diag_id) != antiDiags.end())
+                continue;
+            
+            cols.insert(col);
+            diags.insert(diag_id);
+            antiDiags.insert(anti_diag_id);
+            board[row][col] = 'Q';
+            
+            
+            solve(board, row+1, cols, diags, antiDiags);
+
+            cols.erase(col);
+            diags.erase(diag_id);
+            antiDiags.erase(anti_diag_id);
+            board[row][col] = '.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         if(n == 0)
             return {};
         vector<string> board(n, string(n, '.')); 
-        //For, n = 3, board = {"...", "...", "..."} initially
         
-        solve(board, 0);
+        int start_row = 0;
+        unordered_set<int> cols;
+        unordered_set<int> diags;
+        unordered_set<int> antiDiags;
+        solve(board, start_row, cols, diags, antiDiags);
         
         return result;
     }
