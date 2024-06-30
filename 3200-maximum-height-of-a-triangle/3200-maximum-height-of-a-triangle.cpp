e0684f59e5e0dcf977382ca09bec1df6359c16fd
Samriddh <56023229+saketbyte@users.noreply.github.com>
2024-06-30 16:36:28 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (100.00%) - LeetHub

--- 3200-maximum-height-of-a-triangle/3200-maximum-height-of-a-triangle.cpp
+++ 3200-maximum-height-of-a-triangle/3200-maximum-height-of-a-triangle.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
+  
+    int helper(int a, int b){
+        
+     int n1 = sqrt(a); // number of rows for the a ball
+        int n2 = (-1 + sqrt(1 + 4 * b)) / 2; // number of rows for the b ball 
+
+        // if the odd rows are the limiting factor
+        if(n1 <= n2) return n1 * 2; 
+        
+        // if the even rows are limiting factor, add +1 because one more odd will be there after we're finished will all the even rows
+        else return n2 * 2 + 1; 
+    }
+
+
     int maxHeightOfTriangle(int red, int blue) {
-        return max(solve(red,blue), solve(blue,red));
+        return max(helper(red, blue), helper(blue, red));
     }
-    
-    
-    int solve(int red, int blue){
-        int balls = 1, height = 0;
-        while(true){
-            red -= balls;
-            if(red < 0)
-                break;
-            balls++;
-            height++;
-            blue -= balls;
-            if(blue < 0)
-                break;
-            balls++;
-            height++;
-        }
-        return height;
-    }
-    
 };
