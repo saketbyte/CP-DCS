2937f1aca9c936eda4b1ce61936e59dcb9e87b64
Samriddh <56023229+saketbyte@users.noreply.github.com>
2024-07-13 19:32:33 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (8.55%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,40 +1,55 @@
 class Twitter {
-    long long time;
-    unordered_map<int,set<int>>following;
-    unordered_map<int,vector<pair<int,int>>>tweets;
-  public:
-  
+public:
+    unordered_map<int, unordered_set<int>> friends;
+    unordered_map<int, vector<pair<int,int>>> tweets;
+    int time;
     Twitter() {
-        time=0;
+        time =0;
     }
-
-
-    void postTweet(int userId, int tweetId) {
-        tweets[userId].push_back({time++,tweetId});
+    
+    void postTweet(int uid, int tid) {
+        tweets[uid].push_back({time++,tid});
     }
-
-
-    vector<int> getNewsFeed(int userId) {
-        vector<int>res;
-        priority_queue<pair<int,int>>pq;
-        for(auto it:tweets[userId])pq.push(it);
-        for(int follower:following[userId]){
-            for(auto it:tweets[follower])pq.push(it);
-        }
-        while(!pq.empty() && res.size()<10){
-            res.push_back(pq.top().second);
+    
+     void follow(int follower, int fren) {
+        
+         friends[follower].insert(fren);
+    }
+    
+    void unfollow(int follower, int fren) {
+        if(friends[follower].find(fren)!=friends[follower].end())
+            friends[follower].erase(fren);}
+    
+    
+     vector<int> getNewsFeed(int uid) {
+        vector<int> feed;
+        priority_queue<pair<int,int>> pq;
+        
+        for(auto it: tweets[uid]) pq.push(it);
+        
+        for(int follower: friends[uid])
+            for(auto it:tweets[follower])
+                pq.push(it);
+        
+        while(!pq.empty() && feed.size()<10)
+        {
+            feed.push_back(pq.top().second);
             pq.pop();
         }
-        return res;
-    }
-
-
-    void follow(int followerId, int followeeId) {
-        following[followerId].insert(followeeId);
-    }
-
-
-    void unfollow(int followerId, int followeeId) {
-        following[followerId].erase(followeeId);
+        
+        return feed;
     }
 };
+    
+   
+    
+   
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
